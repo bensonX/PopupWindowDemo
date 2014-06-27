fe34019a6208ded0eb3bfa4b9b6f8a576aeeb551
yehoubin <yy@yydeMacBook-Pro.local>
2014-06-27 12:49:11 +0800

    Revert "test"
    This reverts commit 1ad842eead1b4ff2d3faf6f048842f2864c7c3cc.

---
+++ test/Air.java
@@ -0,0 +1,44 @@
+package com.yy.environmentalprotection.data.bean;
+
+import java.io.Serializable;
+
+public class Air implements Serializable {
+	private String datatime;//监测时间
+	private String pointname;//监测点
+	private String aqi;//AQI
+	private String airquality;//空气质量状况
+	private String pollutant;//首要污染物
+	public String getDatatime() {
+		return datatime;
+	}
+	public void setDatatime(String datatime) {
+		this.datatime = datatime;
+	}
+	public String getPointname() {
+		return pointname;
+	}
+	public void setPointname(String pointname) {
+		this.pointname = pointname;
+	}
+	public String getAqi() {
+		return aqi;
+	}
+	public void setAqi(String aqi) {
+		this.aqi = aqi;
+	}
+	public String getAirquality() {
+		return airquality;
+	}
+	public void setAirquality(String airquality) {
+		this.airquality = airquality;
+	}
+	public String getPollutant() {
+		return pollutant;
+	}
+	public void setPollutant(String pollutant) {
+		this.pollutant = pollutant;
+	}
+	
+	
+	
+}
